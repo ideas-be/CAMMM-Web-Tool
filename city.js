a116f4a613d33c094ff03ee7c8b80e35e36f4268
Firdous <fn24x7@gmail.com>
2021-03-03 15:08:49 -0500

    Testing JSON and HTML Table

---
+++ city.js
@@ -0,0 +1,13 @@
+class City{
+    constructor(name, NumTransportSystem, NumBusStops, NumRailStations, NumMetroStations, NumBoroughs, AreaSqKm, PopulationMillion, DensityPersonSqKm){
+        this.name=name;
+        this.NumTransportSystem=NumTransportSystem;
+        this.NumBusStops=NumBusStops;
+        this.NumRailStations=NumRailStations;
+        this.NumMetroStations=NumMetroStations;
+        this.NumBoroughs=NumBoroughs;
+        this.AreaSqKm=AreaSqKm;
+        this.PopulationMillion=PopulationMillion;
+        this.DensityPersonSqKm=DensityPersonSqKm;
+    }
+}
