9c2cbe9a83912c9ffec83d37023e67c277eb458c
Firdous <fn24x7@gmail.com>
2021-03-31 11:31:42 -0400

    more console logging city list

--- script.js
+++ script.js
@@ -53,6 +53,8 @@ function initJson(jsonObj, selAnalysis){  // This creates a function to pull out
 function dropdownCities(){
 	var cityHTML = "<option disabled selected>Select City</option>";
 	console.log(typeof (myJson["City"]));
+	console.log( myJson["City"]);
+
 	// for (const [i, value] of myJson["City"].entries()) {
 	// 	console.log("i:", i, "value:", value);
 	// }
