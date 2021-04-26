e084cd8b15369cfe382e30c986ffee7deb368fa3
Firdous <fn24x7@gmail.com>
2021-04-26 16:21:40 -0400

    Injecting toggle in HTML from City class

--- city.js
+++ city.js
@@ -10,6 +10,7 @@ class City{
         this.city = city;
         this.cityNum = cityNum;
         this.readCityJson();
+        this.injectToggleTest();
     }
     readCityJson(){
         const {city, cityNum} = this;
@@ -21,6 +22,11 @@ class City{
         this.cityCoords = cityCoords;
         this.cityZoom = cityZoom;
         console.log("cityCoords", cityCoords,"cityZoom",cityZoom);
+    }
+    injectToggleTest(){
+        console.log("Injecting toggle in HTML");
+        toggleHTML="<input type=\"checkbox\" id=\"toggBtntest\" onchange=\"City1.getToggleTest();\">"+"<span class=\"slider round\"></span>";
+        document.getElementById("toggleTest").innerHTML = toggleHTML;
     }
     getToggleTest(){
 		selToggle = document.getElementById(toggBtntest).checked;
