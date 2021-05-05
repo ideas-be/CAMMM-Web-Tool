57a032d45e411e5274a09ae04acf12e441079a06
Firdous <fn24x7@gmail.com>
2021-05-05 11:40:18 -0400

    Removing loadlayer from script js

--- script.js
+++ script.js
@@ -12,25 +12,25 @@ var cityContainer = "";
 // 	document.getElementById(containerId).innerHTML = formHTML;
 // }
 
-function loadLayer(currentLayer){
-// set up the corresponding toggle button for each layer
+// function loadLayer(currentLayer){
+// // set up the corresponding toggle button for each layer
 
-	var visibility = map.getLayoutProperty(currentLayer, 'visibility');
+// 	var visibility = map.getLayoutProperty(currentLayer, 'visibility');
  
-// toggle layer visibility by changing the layout object's visibility property
-if (visibility === 'visible') {
-	map.setLayoutProperty(currentLayer, 'visibility', 'none');
-	console.log(currentLayer+ " turned off");
-	map.setLayoutProperty(prevLayer, 'visibility', 'visible');
-	} else {
-		map.setLayoutProperty(currentLayer, 'visibility', 'visible');
-		console.log(currentLayer+" turned on");
-		map.setLayoutProperty(prevLayer, 'visibility', 'none');
-		}
+// // toggle layer visibility by changing the layout object's visibility property
+// if (visibility === 'visible') {
+// 	map.setLayoutProperty(currentLayer, 'visibility', 'none');
+// 	console.log(currentLayer+ " turned off");
+// 	map.setLayoutProperty(prevLayer, 'visibility', 'visible');
+// 	} else {
+// 		map.setLayoutProperty(currentLayer, 'visibility', 'visible');
+// 		console.log(currentLayer+" turned on");
+// 		map.setLayoutProperty(prevLayer, 'visibility', 'none');
+// 		}
 
-		prevLayer = currentLayer;
+// 		prevLayer = currentLayer;
 
-}
+// }
 
 
 function CityDataDisplay(selCity){
