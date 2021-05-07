b1e08c9b0a2e2e9a97e7bacc5b4473e678fc8cec
Firdous <fn24x7@gmail.com>
2021-05-07 10:38:29 -0400

    Troubleshooting declaration of radio list
    Co-authored-by: Omar Ortiz Meraz <omar.ortizmeraz@mail.concordia.ca>

--- city.js
+++ city.js
@@ -5,60 +5,59 @@ var myJson;
 var shortURL = 'mapbox://styles/carmela-cucuzzella/';
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2FybWVsYS1jdWN1enplbGxhIiwiYSI6ImNrZThua3M2djF0MmkzMnFodmlncjU1MzUifQ.kQ7CmjkzU5V5-sY7WFkzmg';
 
-function newJson(obj){
-    myJson=obj;
+function newJson(obj) {
+    myJson = obj;
     console.log("Initializing new Json obj");
     dropdownCities();
 }
 
-function getCity(city, num){
-    if(num==1){
+function getCity(city, num) {
+    if (num == 1) {
         City1 = city;
         console.log("This is City 1");
         console.log(City1);
-    }else if(num==2){
+    } else if (num == 2) {
         City2 = city;
         console.log("This is City 2");
         console.log(City2);
     }
 }
 
-function getSelectedCity(num){
-    var citylist="cityList"+num;
+function getSelectedCity(num) {
+    var citylist = "cityList" + num;
     var selectedCity = document.getElementById(citylist).value;
 
-    if(selectedCity != "Select City"){
-        
+    if (selectedCity != "Select City") {
+
         let city = new City(selectedCity, num, myJson["City"][selectedCity]);
         getCity(city, num);
         // readCityJson(selectedCity, 2);
         // CityDataDisplay(selectedCity);
 
-        var mapdivId = "map"+num+"div";
+        var mapdivId = "map" + num + "div";
         var mapdiv = document.getElementById(mapdivId);
-        
+
         var mapdisplay = mapdiv.style.display;
-        if(mapdisplay == 'none')
-            {
-                mapdiv.style.display = 'block';
-            }
+        if (mapdisplay == 'none') {
+            mapdiv.style.display = 'block';
+        }
     }
 }
 
-function dropdownCities(){
-	var cityHTML = "<option disabled selected>Select City</option>";
+function dropdownCities() {
+    var cityHTML = "<option disabled selected>Select City</option>";
 
-	for(key in myJson["City"]){
-		cityHTML += "<option value=\"" + myJson["City"][key].name + "\">"+myJson["City"][key].name +"</option>";
-	}
+    for (key in myJson["City"]) {
+        cityHTML += "<option value=\"" + myJson["City"][key].name + "\">" + myJson["City"][key].name + "</option>";
+    }
 
-	document.getElementById("cityList1").innerHTML = cityHTML;
-	document.getElementById("cityList2").innerHTML = cityHTML;
+    document.getElementById("cityList1").innerHTML = cityHTML;
+    document.getElementById("cityList2").innerHTML = cityHTML;
 
 }
 
-class City{
-    constructor(city, cityNum, cityJson){
+class City {
+    constructor(city, cityNum, cityJson) {
         this.city = city;
         this.cityNum = cityNum;
         this.cityJson = cityJson;
@@ -67,44 +66,43 @@ class City{
         this.getToggleTest();
     }
     readCityJson() {
-        const { city, cityNum, cityJson} = this;
-        console.log("city", city,"cityNum",cityNum);
+        const { city, cityNum, cityJson } = this;
+        console.log("city", city, "cityNum", cityNum);
         console.log(cityJson);
 
         let cityCoords = cityJson.Coords;
         let cityZoom = cityJson.Zoom;
-        
+
         this.cityCoords = cityCoords;
         this.cityZoom = cityZoom;
-        console.log("cityCoords", cityCoords,"cityZoom",cityZoom);
+        console.log("cityCoords", cityCoords, "cityZoom", cityZoom);
     }
-    injectToggleHTML(){
+    injectToggleHTML() {
         const { cityNum } = this;
         console.log("Injecting toggle in HTML");
-        var toggleHTML="<p class=\"toggleText\">Direct"+
-			"<label class=\"switch\" >"+
-            "<input type=\"checkbox\" id=\"toggBtn" + cityNum+"\" onchange=\"City"+cityNum+".getToggleTest();\">"+
-                        "<span class=\"slider round\"></span>"+
-			"</label>     Node"+
-		"</p>";
+        var toggleHTML = "<p class=\"toggleText\">Direct" +
+            "<label class=\"switch\" >" +
+            "<input type=\"checkbox\" id=\"toggBtn" + cityNum + "\" onchange=\"City" + cityNum + ".getToggleTest();\">" +
+            "<span class=\"slider round\"></span>" +
+            "</label>     Node" +
+            "</p>";
         var toggleID = "toggleCity" + cityNum;
         document.getElementById(toggleID).innerHTML = toggleHTML;
     }
 
-    getToggleTest(){
-        const { cityNum, cityJson} = this;
+    getToggleTest() {
+        const { cityNum, cityJson } = this;
         var toggleID = "toggBtn" + cityNum;
         var selAnalysis = document.getElementById(toggleID).checked;
-		console.log("selToggle:", selAnalysis);
-        let cityURL="";
+        console.log("selToggle:", selAnalysis);
+        let cityURL = "";
         let ListOfLayers = [];
-        if(selAnalysis){
+        if (selAnalysis) {
             console.log("Loading map type: Node");
             cityURL = cityJson.NodeStyleURL;
             ListOfLayers = cityJson.NodeLayers;
 
-            // TODO Load radio buttons
-        }else{
+        } else {
             console.log("Loading map type: Direct");
             cityURL = cityJson.DirectStyleURL;
             ListOfLayers = cityJson.DirectLayers;
@@ -116,39 +114,39 @@ class City{
         this.radioButtons();
     }
 
-    loadLayer(){
-        const{map, ListOfLayers} =this;
-        let radioList = [];
+    loadLayer() {
+        const { map, ListOfLayers } = this;
+        var radioList = [];
         // console.log("ListOfLayers.length", ListOfLayers.length);
         for (const value of ListOfLayers.entries()) {
-        // for(let i=0;i++;i<ListOfLayers.length){
-            var buttonStatus=document.getElementById(value).checked;
+            // for(let i=0;i++;i<ListOfLayers.length){
+            var buttonStatus = document.getElementById(value).checked;
             console.log("buttonStatus", buttonStatus);
             radioList.push(buttonStatus);
-        // TODO Make this for loop work properly BEGIN HERE NEXT SESSION !!!!!
+            // TODO Make this for loop work properly BEGIN HERE NEXT SESSION !!!!!
         }
-        this.radioList=radioList;
+        this.radioList = radioList;
         console.log("This is all the radio buttons");
         console.log(this.radioList);
     }
 
     radioButtons() {
         const { cityNum, ListOfLayers } = this;
-        var formHTML ="";
+        var formHTML = "";
         var NameOfQueries = ["Centrality Degree", "Closeness"];
         for (const [i, value] of ListOfLayers.entries()) {
             this.value = value;
-            formHTML += "<input type=\"radio\" name=\"mapRadios\" id=\"" + value + "\" value=\"" + value + "\" onclick=\"" + "City"+cityNum+".loadLayer();" + "\">" +
+            formHTML += "<input type=\"radio\" name=\"mapRadios\" id=\"" + value + "\" value=\"" + value + "\" onclick=\"" + "City" + cityNum + ".loadLayer();" + "\">" +
                 "<label for=\"" + value + "\">" + NameOfQueries[i] + "</label>"
         }
-        var containerId = "radioForm"+cityNum;	
+        var containerId = "radioForm" + cityNum;
         document.getElementById(containerId).innerHTML = formHTML;
     }
 
     loadMap() {
         const { cityNum, cityCoords, cityZoom, cityURL } = this;
 
-        var mapContainer="map"+cityNum;
+        var mapContainer = "map" + cityNum;
         let map = new mapboxgl.Map({
             container: mapContainer,
             style: shortURL + cityURL,
@@ -169,9 +167,9 @@ class City{
 //     let currentButton = document.getElementById(value).checked;
 //     let prevLayer = currentLayer;
 //     // set up the corresponding toggle button for each layer
-    
+
 //     var visibility = map.getLayoutProperty(currentLayer, 'visibility');
-     
+
 //     // toggle layer visibility by changing the layout object's visibility property
 //     if (visibility === 'visible') {
 //         map.setLayoutProperty(currentLayer, 'visibility', 'none');
@@ -182,7 +180,7 @@ class City{
 //             console.log(currentLayer+" turned on");
 //             map.setLayoutProperty(prevLayer, 'visibility', 'none');
 //             }
-    
+
 //     prevLayer = currentLayer;
-    
+
 // }
