62c4e11b6c0d5ec3da5144bcfc3ea4fa7c32aedd
Firdous <fn24x7@gmail.com>
2021-05-11 13:47:22 -0400

    Changing js of info popup

--- info_popup.js
+++ info_popup.js
@@ -1,11 +1,16 @@
-$(window).load(function () {
-    $(".trigger_info_popup").click(function () {
-        $('.hover_info_popup').show();
-    });
-    $('.hover_info_popup').click(function () {
-        $('.hover_info_popup').hide();
-    });
-    $('.popupCloseButton').click(function () {
-        $('.hover_info_popup').hide();
-    });
-});
+// $(window).load(function () {
+//     $(".trigger_info_popup").click(function () {
+//         $('.hover_info_popup').show();
+//     });
+//     $('.hover_info_popup').click(function () {
+//         $('.hover_info_popup').hide();
+//     });
+//     $('.popupCloseButton').click(function () {
+//         $('.hover_info_popup').hide();
+//     });
+// });
+
+function show_popup() {
+    var infoPopUp = document.getElementsByClassName("hover_info_popup");
+    infoPopUp.classList.toggle("show");
+}
