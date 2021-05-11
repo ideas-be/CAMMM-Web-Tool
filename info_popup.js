b1797e1fe82592ade1291b35a9504340ef36d738
Firdous <fn24x7@gmail.com>
2021-05-11 14:01:12 -0400

    Getting popup style display to toggle visibility

--- info_popup.js
+++ info_popup.js
@@ -12,6 +12,13 @@
 
 function show_popup() {
     var infoPopUp = document.getElementById("hover_info_popup");
-    infoPopUp.classList.toggle("show");
+
+    // infoPopUp.classList.toggle("show");
+    var popupDisplay = infoPopUp.style.display;
+    if (popupDisplay == 'none') {
+        infoPopUp.style.display = 'block';
+    } else if (popupDisplay == 'block') {
+        infoPopUp.style.display = 'none';
+    }
     console.log("This is the showpopup function");
 }
