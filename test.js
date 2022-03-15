1a573551e4bfb6782e686028f1d489b769d9c4e9
Firdous Nizar <fn24x7@gmail.com>
2022-03-15 16:11:46 -0400

    Adding test content and question boxes to inforgraphic animation
    Co-authored-by: Omar Ortiz Meraz <omar.ortizmeraz@mail.concordia.ca>

--- test.js
+++ test.js
@@ -1,16 +1,5 @@
-const boxes = document.querySelectorAll('.box');
+const questions = ["Why are some cities better at public transport use?", "How?", "What does this tool contain?", "What is CAMMM Atlas web application used for?", "What are the queries?"];
+
+const appQuestions = ["Could it be the density of the transport system?", "Could it be the number of connections per stop?", "Could it be the overall shape of the system?", "Could it be the distance between stops?", "Could it be the number of amenities near stops?"];
 
 
-window.addEventListener('scroll', scrolling);
-
-function scrolling() {
-    const triggerBottom = window.innerHeight * (4 / 5);
-    boxes.forEach(box => {
-        const boxTop = box.getBoundingClientRect().top;
-        if (triggerBottom > boxTop) {
-            box.classList.add('show');
-        } else {
-            box.classList.remove('show')
-        }
-    })
-}
