67980b61c07bb26778a1eb53a25be66b1e66c21e
Firdous Nizar <48064033+spatialretrace@users.noreply.github.com>
2022-09-26 17:01:12 -0400

    Adding fill extrustion polygons to service geojson

--- Scripts/JavaScript/sidebar.js
+++ Scripts/JavaScript/sidebar.js
@@ -1,14 +1,27 @@
+var myJson1, myJson2;
+
+function newJson1(obj) {
+    myJson1 = obj;
+    console.log("Initializing new POI Json obj");
+    // console.log(myJson1);
+}
+function newJson2(obj) {
+    myJson2 = obj;
+    console.log("Initializing new Services Json obj");
+    // console.log(myJson2);
+}
+
 var isOpened = true;
 
-const openSidebar = (desc) => {
+const openSidebar = (desc, stview) => {
     if (isOpened) {
-        console.log(desc);
+        // console.log(desc);
         var sidebarDiv = document.getElementById("mySidebar");
         // var windowWidth = window.MediaQueryList.windowWidth;
-        sidebarDiv.style.width = "250px";
+        sidebarDiv.style.width = "300px";
         // toString(windowWidth * 0.3);
-        document.getElementById("main").style.marginRight = "250px";
-        sidebarDiv.innerHTML = desc;
+        document.getElementById("main").style.marginRight = "300px";
+        sidebarDiv.innerHTML = stview + desc;
         isOpened = false;
     } else {
         var sidebarDiv = document.getElementById("mySidebar");
