f0ab510369873dacd64308c68194a6597550fe6b
Firdous Nizar <48064033+spatialretrace@users.noreply.github.com>
2023-10-23 09:59:09 -0400

    Displaying current demo data values in table

--- Scripts/JavaScript/population.js
+++ Scripts/JavaScript/population.js
@@ -6,19 +6,22 @@ function getPopJson(jsonData) {
 }
 
 function displayPopData() {
-    var popDemographicData = popJson.features[0].demographic_data;
+    var MaleDemographicData = popJson.features[0].demographic_data.Males_demoData;
+    var FemaleDemographicData = popJson.features[0].demographic_data.Females_demoData;
 
-    console.log(popDemographicData);
+    // console.log(popDemographicData);
     // console.log("Printing population demographic data: ");
     var popDataHTML = "<table>";
-    for (demoData in popDemographicData.Males_demoData) {
+    for (demoData in MaleDemographicData) {
         //     console.log(demoData, ": ", popDemographicData[demoData]);
-        popDataHTML += "<tr><td>" + demoData + "</td>" + "<td> : </td><td>" + popDemographicData.Males_demoData[demoData] + "</td></tr>";
+        popDataHTML += "<tr><td>" + demoData + "</td>" + "<td> : </td><td>" + MaleDemographicData[demoData] + "</td></tr>";
+        // TODO: Make bar graph div
+        // style=\"width:" + MaleDemographicData[demoData] * 0.025 + "px; height:20px; background-color:#d81b60;\"
     }
     popDataHTML += "</table><br><em>Females: </em><table>";
-    for (demoData in popDemographicData.Females_demoData) {
+    for (demoData in FemaleDemographicData) {
         //     console.log(demoData, ": ", popDemographicData[demoData]);
-        popDataHTML += "<tr><td>" + demoData + "</td>" + "<td> : </td><td>" + popDemographicData.Females_demoData[demoData] + "</td></tr>";
+        popDataHTML += "<tr><td>" + demoData + "</td>" + "<td> : </td><td>" + FemaleDemographicData[demoData] + "</td></tr>";
     }
 
     document.getElementById("borough-query-info").innerHTML = "<em>Males: </em>" + popDataHTML;
