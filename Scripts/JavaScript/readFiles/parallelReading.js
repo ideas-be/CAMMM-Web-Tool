95034381f2710ebc486c5682d9528f6fba120c17
Firdous Nizar <48064033+spatialretrace@users.noreply.github.com>
2024-05-21 16:03:49 -0400

    Fixing read file errors for Montreal and Gatineau

--- Scripts/JavaScript/readFiles/parallelReading.js
+++ Scripts/JavaScript/readFiles/parallelReading.js
@@ -1,32 +1,56 @@
+var files = [];
+
 function loadJSONFilesInParallel(files) {
     return Promise.all(files.map(file => fetch(file).then(response => response.json())));
 }
 
 // Usage
-// TODO: Fix the file path for all cities
-const files = ["Data/Montreal/population_borough.json", "Data/Montreal/city.json", "Data/Montreal/borough.geojson", "Data/Montreal/Lines.geojson"];
+// TODO: Come back for Lines geojson when all cities are updated
+const fileNames = ["population_borough.json", "city.json", "borough.geojson"
+    // , "Lines.geojson"
+];
+
+var cityName = "";
+function getCityName() {
+    cityName = document.getElementById("city-name").innerHTML;
+    console.log("CITY FILES TO BE READ IS: ", cityName);
+}
+
+setTimeout(getCityName, 10);
+setTimeout(generateFileList, 15);
+
+function generateFileList() {
+    var tempPath = "Data/" + cityName + "/";
+    for (file in fileNames) {
+        files.push(tempPath + fileNames[file]);
+    }
+    console.log("FILES TO BE READ ARE: ", files);
+}
 
 var parallelFiles;
-loadJSONFilesInParallel(files)
-    .then(dataArray => {
-        // Process dataArray containing JSON data of all files
-        console.log("All files loaded");
-        // console.log(dataArray);
+setTimeout(function () {
+    loadJSONFilesInParallel(files)
+        .then(dataArray => {
+            // Process dataArray containing JSON data of all files
+            console.log("All files loaded");
+            // console.log(dataArray);
 
-        // Continue with your application logic
-        parallelFiles = dataArray;
-        console.log("Parallel Files read are: ", parallelFiles);
-        // loading population JSON file
-        getPopJson(parallelFiles[0]);
-        // loading city JSON file
-        getCityJSON(parallelFiles[1]);
-        // loading borough GeoJSON file
-        // displayBoroughs(parallelFiles[2]);
-        getBoroughJSON(parallelFiles[2]);
-        // loading lines GeoJSON file
-        getLinesJSON(parallelFiles[3]);
-    })
-    .catch(error => {
-        console.error("Error loading JSON files:", error);
-        // Handle error if necessary
-    });
+            // Continue with your application logic
+            parallelFiles = dataArray;
+            console.log("Parallel Files read are: ", parallelFiles);
+            // loading population JSON file
+            getPopJson(parallelFiles[0]);
+            // loading city JSON file
+            getCityJSON(parallelFiles[1]);
+            // loading borough GeoJSON file
+            // displayBoroughs(parallelFiles[2]);
+            getBoroughJSON(parallelFiles[2]);
+            // loading lines GeoJSON file
+            getLinesJSON(parallelFiles[3]);
+        })
+        .catch(error => {
+            console.error("Error loading JSON files:", error);
+            // Handle error if necessary
+        });
+
+}, 20);
